710eab1512e557ea684125c67ba4da926bf7ec18
khadija4 <atalatakhadija4@gmail.com>
2024-11-10 14:24:35 +0000

    c

---
+++ portfolio.js
@@ -0,0 +1,20 @@
+let sections = document.querySelectorAll('section')
+let navlinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = secoffheight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(
+                links => {
+                    links.classlist.remove('active');
+                    document.querySelector('hader nav a[href*=' + id + ']').classlist.add('active');
+                }
+            )
+        }
+    })
+}
