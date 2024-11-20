f5089f0d83751de296715cf9cde0dcdfbf49154a
khadija4 <atalatakhadija4@gmail.com>
2024-11-20 11:22:13 +0000

    g

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 let sections = document.querySelectorAll('section')
-let navlinks = document.querySelectorAll('header nav a')
+let navlinks = document.querySelectorAll('navbar a')
 
 window.onscroll = () => {
     sections.forEach(sec => {
